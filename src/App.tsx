69e9b6b1eeb40eed2aa4fdb987e16724805950b0
gcastro98 <si.guillecastro@gmail.com>
2023-03-31 20:23:48 +0200

    Initial commit without gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Loader, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense } from 'react';
 import './App.css';
+import { TeslaCharacter } from './Characters/TeslaCharacter';
+import { ChessBoard } from './Rooms/ChessBoard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Canvas
+        orthographic
+        shadows
+        camera={{
+          position: [-674.32, 401.86, 878.18],
+          near: -10000,
+          far: 10000,
+          zoom: 1.5 * 100,
+        }}
+        style={{
+          opacity: 1,
+          transition: "opacity 200ms ease-in-out",
+        }}
+      >
+        <OrbitControls />
+        <color args={["#2114db"]} attach="background" />
+
+        <Suspense fallback={null}>
+            {/* <RoomClass /> */}
+            <ChessBoard  />
+            <TeslaCharacter />
+            {/* <RoomLibrary /> */}
+            {/* <RoomWorkshop /> */}
+        </Suspense>
+        <gridHelper
+          args={[200, 100, "#1100ff", "#1100ff"]}
+          position={[0, -1.26, 0]}
+        />
+        <gridHelper
+          args={[200, 1000, "#1100ff", "#1100ff"]}
+          position={[0, -1.27, 0]}
+        />
+        
+      </Canvas>
+      <Loader />
+    </>
   );
 }
 
