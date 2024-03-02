6657da1bd9ff9c226d9012a506978743290efd93
gcastro98 <si.guillecastro@gmail.com>
2024-03-02 21:22:12 +0100

    Fix ambient lights and evironment

--- src/Scene/GameScene.tsx
+++ src/Scene/GameScene.tsx
@@ -1,4 +1,4 @@
-import './Styles/GameScene.scss'
+import "./Styles/GameScene.scss";
 import { Canvas } from "@react-three/fiber";
 import { Board } from "./Models/Board/Board";
 import { Class } from "./Models/Rooms/Class";
@@ -12,7 +12,7 @@ import { IStatusPlayer } from "../Firebase/Models/IUser";
 import { Observatory } from "./Models/Rooms/Observatory";
 import { Suspense, useContext } from "react";
 import { TimeMachine } from "./Models/Rooms/TimeMachine";
-import { BakeShadows, Loader, OrbitControls } from "@react-three/drei";
+import { BakeShadows, Environment, Loader, OrbitControls, useEnvironment } from "@react-three/drei";
 
 export function GameScene() {
   const { game, users, userId } = useContext(GameContext);
@@ -28,10 +28,15 @@ export function GameScene() {
           opacity: 1,
           transition: "opacity 200ms ease-in-out",
         }}
+        
       >
         <color attach="background" args={["#202030"]} />
         <fog attach="fog" args={["#202030", 1, 45]} />
-
+        <Environment
+          files="assets/environment.hdr"
+          background={true}
+          blur={0.8}
+        />
         <OrbitControls
           target={[13, 0, -13]}
           // autoRotate
@@ -40,9 +45,9 @@ export function GameScene() {
           // minPolarAngle={Math.PI / 6}
           // maxPolarAngle={Math.PI - Math.PI / 6}
         />
-        <ambientLight intensity={0.25} />
+        {/* <ambientLight intensity={0.25} /> */}
         <BakeShadows />
-        <Suspense >
+        <Suspense>
           <Class />
           <Board position={[0, 0, 0]} />
           <Library />
@@ -64,7 +69,6 @@ export function GameScene() {
             })}
         </Suspense>
       </Canvas>
-     
     </>
   );
 }
--- src/Scene/Models/Modifiers.ts
+++ src/Scene/Models/Modifiers.ts
@@ -1,2 +1,2 @@
-export const SCENE_LIGHT_INTENSITY = 0.025
+export const SCENE_LIGHT_INTENSITY = 0.05
 export const REDUCE_SCALE_DOOR = 0.55;
